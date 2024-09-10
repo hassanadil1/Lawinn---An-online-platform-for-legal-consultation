dc8cd9735e704ce36aa83c6e4718d04cf6d30175
hassanadil1 <l226543@lhr.nu.edu.pk>
2024-09-10 23:03:44 +0500

    Footer Component Added.

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -23,7 +23,7 @@ export function InputField() {
     <div className="h-[30rem] flex flex-col justify-center  items-center px-4">
       <h2 className="bg-clip-text text-transparent bg-gradient-to-b from-neutral-900 to-neutral-700 dark:from-neutral-600 dark:to-white text-3xl md:text-4xl lg:text-7xl font-sans py-4 md:py-10 relative z-20 font-bold tracking-tight">
           Ask Pybot
-        </h2>
+      </h2>
       <PlaceholdersAndVanishInput
         placeholders={placeholders}
         onChange={handleChange}
